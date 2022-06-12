cb99c7648b8f6e10be1c90e3d591a4e73e5801d3
Vincent2399 <105112322+Vincent2399@users.noreply.github.com>
2022-06-12 20:13:55 +0200

    Integration

---
+++ public/publication.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Publication</title>
+</head>
+<link rel="stylesheet" href="css/stylesheet.css">
+<link rel="stylesheet" href="css/publication.css">
+<body>
+<img class="banniereCentree" src="../src/lib/abstract-cloud-banner-background.jpg">
+
+<table>
+    <caption>Titre de la publication</caption>
+
+    <caption>Contentu de la publication ....................
+        ..............................................
+        ..............................................
+        .............................................
+        ......................................</caption>
+
+    <tr>
+        <td>Case J'aime</td>
+        <td>Case Commenter</td>
+    </tr>
+</table>
+
+</body>
+</html>
---
+++ public/publier.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Publier</title>
+</head>
+<link rel="stylesheet" href="css/stylesheet.css">
+<body>
+<img class="banniereCentree" src="../src/lib/abstract-cloud-banner-background.jpg">
+
+
+</body>
+</html>
