46897e0e4a29f17241101d924e08e9e24e5461fc
Vincent2399 <105112322+Vincent2399@users.noreply.github.com>
2022-06-19 18:26:58 +0200

    modif fond page

---
+++ publication.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Publication</title>
+</head>
+<link rel="stylesheet" href="css/stylesheet.css">
+<link rel="stylesheet" href="css/publication.css">
+<body style="background-color:grey;">
+<img class="banniereCentree" src="../src/lib/abstract-cloud-banner-background.jpg">
+
+<table style="width:300px; height:100px;">
+    <caption>Titre de la publication</caption>
+
+    <caption>Contentu de la publication ....................
+        ..............................................
+        ..............................................
+        .............................................
+        ......................................</caption>
+
+    <tr>
+        <td>Case J'aime</td>
+        <td>Case Commenter</td>
+    </tr>
+</table>
+
+</body>
+</html>
---
+++ publier.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Publier</title>
+</head>
+<link rel="stylesheet" href="css/stylesheet.css">
+<body style="background-color:grey;">
+<img class="banniereCentree" src="../src/lib/abstract-cloud-banner-background.jpg">
+
+
+</body>
+</html>
