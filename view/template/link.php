9f3b329b01e1e262621f06e814690e13c855231c
JulienOo <julien.pernet@live.fr>
2022-10-20 18:32:47 +0200

    correction miss click

--- view/template/link.php
+++ view/template/link.php
@@ -1,6 +1,6 @@
 <?php ob_start(); ?>
 
-    <link rel="stylesheet" href="/public/css/banniereEtMenu.css">
+    <link rel="stylesheet" href="public/css/banniereEtMenu.css">
 
 <?php $defaultLink = ob_get_clean(); ?>
 
